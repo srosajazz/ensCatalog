c4d723b3848d5c740961df76af3ccd34c626079c
Sergio Rosa <scarlosjazz@gmail.com>
2021-12-17 17:33:11 -0500

    backend & front app

---
+++ backend/src/main/java/com/enscatalog/ensapp/EnsAppApplication.java
@@ -0,0 +1,13 @@
+package com.enscatalog.ensapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/enscatalog/ensapp/EnsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enscatalog.ensapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
